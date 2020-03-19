28f1e11261e94910d311585aa3be4e3d59d07a4b
vlad3k <vlad3k@bk.ru>
2020-03-20 00:48:20 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = Object.entries(MORSE_TABLE);
+    const vocabulary = new Map();
+
+    arr.forEach(item => vocabulary.set(toDigits(item[0]), item[1]));
+
+    let num = Math.ceil(expr.length / 10);
+    let chunks = new Array(num);
+
+    for (let i = 0, j = 0; i < num; i += 1, j += 10) {
+        chunks[i] = expr.substr(j, 10);
+    }
+
+    return chunks.map(item => {
+        if (item === '**********') return ' ';
+        return vocabulary.get(item);
+    }).join('');
+}
+
+function toDigits(str) {
+    const resArr = str.split('').map(char => {
+        if (char === '.') return '10';
+        if (char === '-') return '11';
+    });
+    if (resArr.length === 10) return resArr.join('');
+    const num = 10 - (resArr.length * 2);
+    const nullArray = new Array(num).fill('0');
+    return nullArray.concat(resArr).join('');
 }
 
 module.exports = {
